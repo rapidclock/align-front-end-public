d273a6d908332202a8ac02f8eeb91b475e2a0c5f
chizhang1991 <gongchangchi@hotmail.com>
2018-03-17 12:01:17 -0700

    filter mobile view

--- src/components/student_filter.jsx
+++ src/components/student_filter.jsx
@@ -9,18 +9,23 @@ class StudentFilter extends React.Component {
 		this.state = {
 			selCoops:'',
 			selDegrees:'',
-			selUni:'',
-			selYears:''
+		  selUni:'',
+      selYears:'',
+			isExpand: false,
 		};
 
 		this.handleSubmit = this.handleSubmit.bind(this);
 		this.handleChange = this.handleChange.bind(this);
+		this.handleExpand = this.handleExpand.bind(this);
 	}
 
 	handleSubmit(event){
 		//todo
 		console.log(event.target);
 		event.preventDefault();
+		this.setState({
+			isExpand: false,
+		})
 	}
 
 	handleChange(event){
@@ -29,7 +34,14 @@ class StudentFilter extends React.Component {
 		event.preventDefault();
 	}
 
+	handleExpand() {
+		this.setState({
+			isExpand: !this.state.isExpand,
+		});
+	}
+
 	render(){
+		const {isMobile} = this.props;
 		//placeholder code
 		var coop_title = "Coop";
 		var degree_title = "Degree Subject";
@@ -52,49 +64,86 @@ class StudentFilter extends React.Component {
 		var year_checked = [false, false, false, false, false];
 		var year_quant = [13, 10, 9, 6, 3];
 
-		return(
+		const filterContent = (
+			<form id="filter_below_fold" onSubmit={this.handleSubmit}>
+				<FilterGroup
+					name="coop_filter_group"
+					title={coop_title}
+					item_arr={coop_arr}
+					checked={coop_checked}
+					quantity={coop_quant}
+				/>
+				<FilterGroup
+					name="degree_filter_group"
+					title={degree_title}
+					item_arr={degree_arr}
+					checked={degree_checked}
+					quantity={degree_quant}
+				/>
+				<FilterGroup
+					name="uni_filter_group"
+					title={uni_title}
+					item_arr={uni_arr}
+					checked={uni_checked}
+					quantity={uni_quant}
+				/>
+				<FilterGroup
+					name="year_filter_group"
+					title={year_title}
+					item_arr={year_arr}
+					checked={year_checked}
+					quantity={year_quant}
+				/>
+				<input type="submit" value="Update" />
+			</form>
+		)
+
+		const mobileView = (
+			<div id="filter_container_mobile">
+				<div id="filter_above_fold">
+					{this.state.isExpand ? (
+						<button
+							className="filter_header_title_mobile filter_header_title_mobile_expand"
+							onClick={this.handleExpand}
+						>
+							Filter Students
+							<svg fill="black" height="24" viewBox="0 0 24 24" width="24"
+								xmlns="http://www.w3.org/2000/svg">
+								<path d="M10 18h4v-2h-4v2zM3 6v2h18V6H3zm3 7h12v-2H6v2z"/>
+								<path d="M0 0h24v24H0z" fill="none"/>
+							</svg>
+						</button>
+					) : (
+						<button className="filter_header_title_mobile" onClick={this.handleExpand}>
+							Filter Students
+							<svg fill="white" height="24" viewBox="0 0 24 24" width="24"
+								xmlns="http://www.w3.org/2000/svg">
+								<path d="M10 18h4v-2h-4v2zM3 6v2h18V6H3zm3 7h12v-2H6v2z"/>
+								<path d="M0 0h24v24H0z" fill="none"/>
+							</svg>
+						</button>
+					)}
+					<div id="filter_contents_container_mobile">
+						{this.state.isExpand ? filterContent : null}
+					</div>
+				</div>
+			</div>
+		)
+
+		const desktopView = (
 			<div id="filter_container">
 				<div id="filter_above_fold">
-					<a className="filter_header_title">Filter Students</a>
+					<a className="filter_header_title">
+						Filter Students
+					</a>
 				</div>
-				<hr/>
-				<form 
-					id="filter_below_fold" 
-					onSubmit={this.handleSubmit}>
-					<FilterGroup
-						name="coop_filter_group"
-						title={coop_title}
-						item_arr={coop_arr}
-						checked={coop_checked}
-						quantity={coop_quant}
-					/>
-					<FilterGroup
-						name="degree_filter_group"
-						title={degree_title}
-						item_arr={degree_arr}
-						checked={degree_checked}
-						quantity={degree_quant}
-					/>
-					<FilterGroup
-					  	name="uni_filter_group"
-						title={uni_title}
-						item_arr={uni_arr}
-						checked={uni_checked}
-						quantity={uni_quant}
-					/>
-					<FilterGroup
-					  name="year_filter_group"
-						title={year_title}
-						item_arr={year_arr}
-						checked={year_checked}
-						quantity={year_quant}
-					/>
-					<input 
-						type="submit" 
-						value="Update" />
-				</form>
+				<hr />
+				<div id="filter_contents_container">
+					{filterContent}
+				</div>
 			</div>
 		);
+		return isMobile ? mobileView : desktopView;
 	}
 }
 
